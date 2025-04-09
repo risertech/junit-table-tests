7baafc72c76c040a98afa78064fc6ec62ac6db8d
Phil Borlin <phil@risertech.com>
2025-04-08 21:07:19 -0600

    Put Test record in test

--- src/test/java/com/risertech/junittabletests/SimpleShould.java
+++ src/test/java/com/risertech/junittabletests/SimpleShould.java
@@ -11,17 +11,17 @@ import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
 public class SimpleShould {
-    // Create a record with the test name and any arguments and return values
-    record IdentityTest(String name, int number) {
-    }
-
     @TestFactory
     Collection<DynamicTest> testIdentity() {
+        // Create a record with the test name and any arguments and return values
+        record Test(String name, int number) {
+        }
+
         // Create a list of Tests
         var tests = Arrays.asList(
-                new IdentityTest("zero", 0),
-                new IdentityTest("min", Integer.MIN_VALUE),
-                new IdentityTest("max", Integer.MAX_VALUE));
+                new Test("zero", 0),
+                new Test("min", Integer.MIN_VALUE),
+                new Test("max", Integer.MAX_VALUE));
 
         // Map over the tests to return a collection of dynamicTests
         return tests.stream().map(test -> dynamicTest(test.name, () -> {
